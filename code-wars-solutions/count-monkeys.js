75e147c17854d5271ac1bbfe4d8f5eb368ec1674
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-03-26 23:12:41 +0300

    counting monkeys

---
+++ code-wars-solutions/count-monkeys.js
@@ -0,0 +1,11 @@
+// count monkeys solution
+function monkeyCount(n) {
+    // your code here
+      let monkeyCount = []
+      let i = 1
+      while(i <= n){
+        monkeyCount.push(i)
+        i += 1
+      }
+      return monkeyCount
+    }
